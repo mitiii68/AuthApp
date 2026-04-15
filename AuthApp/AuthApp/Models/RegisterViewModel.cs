4a381e381354c080804c0aa33f805a425903fc20
Митя <elita700@gmail.com>
2026-04-15 11:21:31 +0600

    Первый коммит: регистрация

---
+++ AuthApp/AuthApp/Models/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace AuthApp.Models
+{
+    public class RegisterVievModel
+    {
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set;  }
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+    }
+}
---
+++ AuthApp/AuthApp/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace AuthApp.Models
+{ public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsConfirmed { get; set; }
+        public string ConfirmationToken { get; set; }
+        public DateTime CreateAt { get; set; }
+    }
+}
