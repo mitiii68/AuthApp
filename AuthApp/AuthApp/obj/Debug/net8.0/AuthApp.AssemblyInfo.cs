d782160dc905f6a8d0db3a135e3a1be9b9d6392a
Митя <elita700@gmail.com>
2026-04-27 00:32:11 +0600

    исправил оформление в документах и тегах

--- AuthApp/AuthApp/obj/Debug/net8.0/AuthApp.AssemblyInfo.cs
+++ AuthApp/AuthApp/obj/Debug/net8.0/AuthApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad0b4dedc7425cb9823cc2ab4f83b3b525b30162")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15f0b770bbe55262780443dd4c8f8fdaf9b2cce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
